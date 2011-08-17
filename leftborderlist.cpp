16d876338acd6f82f8c5147d355caa00bb360671
sergey.mikhno <sergey.mikhno@gmail.com>
2011-08-17 19:39:25 +0000

    initial commit

---
+++ leftborderlist.cpp
@@ -0,0 +1,17 @@
+#include "leftborderlist.h"
+
+void LeftBorderList::init(float coord)
+{
+    Figure *fig;
+    for (int i = 0; i < 20; i++)
+    {
+        fig = new Figure(coord, i * 32);
+        fence.push_back(*fig);
+    }
+
+}
+
+LeftBorderList::LeftBorderList()
+{
+    init(-32);
+}
