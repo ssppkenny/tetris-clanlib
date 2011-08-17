7034e2502277f4e88f8dcd1a7a263e4a6e475dae
sergey.mikhno@gmail.com <sergey.mikhno@gmail.com@cc71114b-9358-d925-7132-e8e89601ae36>
2011-08-17 19:26:46 +0000

    initial commit

---
+++ borderlist.cpp
@@ -0,0 +1,39 @@
+#include "borderlist.h"
+
+
+
+BorderList::BorderList()
+{
+
+}
+BorderList::BorderList(float coord)
+{
+
+}
+
+
+void BorderList::addList(std::vector<Figure> list)
+{
+    std::vector<Figure>::iterator iter;
+    for (iter=list.begin(); iter!=list.end(); iter++ )
+    {
+        fence.push_back((*iter));
+    }
+
+
+}
+
+void BorderList::draw(CL_GraphicContext gc)
+{
+    std::vector<Figure>::iterator iter;
+    int i = 0;
+    for (iter=fence.begin(); iter!=fence.end(); iter++ )
+    {
+        i++;
+        (*iter).draw(gc);
+    }
+
+
+}
+
+
