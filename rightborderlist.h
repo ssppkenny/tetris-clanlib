36d8b9f31d9ad991fc2117df06abec19e0c3cc3a
sergey.mikhno <sergey.mikhno@gmail.com>
2011-08-17 19:41:46 +0000

    initial commit

---
+++ rightborderlist.h
@@ -0,0 +1,14 @@
+#ifndef RIGHTBORDERLIST_H_INCLUDED
+#define RIGHTBORDERLIST_H_INCLUDED
+
+#include "borderlist.h"
+
+class RightBorderList : public BorderList
+{
+    public:
+       RightBorderList();
+       void init(float coord);
+};
+
+
+#endif // RIGHTBORDERLIST_H_INCLUDED
