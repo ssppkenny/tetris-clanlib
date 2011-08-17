609fa75cfcd3d5ca2d207f974de4170c2611b3f8
sergey.mikhno@gmail.com <sergey.mikhno@gmail.com@cc71114b-9358-d925-7132-e8e89601ae36>
2011-08-17 19:31:35 +0000

    initial commit

---
+++ precomp.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <ClanLib/core.h>
+#include <ClanLib/application.h>
+#include <ClanLib/display.h>
+#include <cmath>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <utility>
+
