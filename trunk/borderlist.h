1db726835242e839a90042a3db4205a33f4c91f3
sergey.mikhno@gmail.com <sergey.mikhno@gmail.com@cc71114b-9358-d925-7132-e8e89601ae36>
2011-08-17 19:37:17 +0000

    initial commit

---
+++ trunk/borderlist.h
@@ -0,0 +1,17 @@
+#ifndef BORDERLIST_H_INCLUDED
+#define BORDERLIST_H_INCLUDED
+#include "precomp.h"
+#include "figure.h"
+
+class BorderList
+{
+    public:
+      std::vector<Figure> fence;
+      BorderList();
+      BorderList(float coord);
+      void draw(CL_GraphicContext gc);
+      void addList(std::vector<Figure>);
+};
+
+
+#endif // BORDERLIST_H_INCLUDED
