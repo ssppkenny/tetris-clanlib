961f35b79f7451924dcae069b44aaa3dd97f7d24
sergey.mikhno@gmail.com <sergey.mikhno@gmail.com@cc71114b-9358-d925-7132-e8e89601ae36>
2011-08-17 19:39:42 +0000

    initial commit

---
+++ trunk/leftborderlist.h
@@ -0,0 +1,13 @@
+#ifndef LEFTBORDERLIST_H_INCLUDED
+#define LEFTBORDERLIST_H_INCLUDED
+#include "borderlist.h"
+
+class LeftBorderList : public BorderList
+{
+    public:
+       LeftBorderList();
+       void init(float coord);
+};
+
+
+#endif // LEFTBORDERLIST_H_INCLUDED
