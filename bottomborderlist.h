c951459d00b0887b3c0ae7ad0e6d16ec7236d2da
sergey.mikhno <sergey.mikhno@gmail.com>
2011-08-17 19:37:47 +0000

    initial commit

---
+++ bottomborderlist.h
@@ -0,0 +1,13 @@
+#ifndef BOTTOMBORDERLIST_H_INCLUDED
+#define BOTTOMBORDERLIST_H_INCLUDED
+
+#include "borderlist.h"
+
+class BottomBorderList : public BorderList
+{
+    public:
+       BottomBorderList();
+       void init(float coord);
+};
+
+#endif // BOTTOMBORDERLIST_H_INCLUDED
